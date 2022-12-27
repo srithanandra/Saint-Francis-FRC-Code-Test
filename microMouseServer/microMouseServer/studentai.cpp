5603d868e2da9bd95ee4da6c8cfb196f3f928207
srithanandra <120761475+srithanandra@users.noreply.github.com>
2022-12-27 11:36:56 -0800

    Update studentai.cpp

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -3,16 +3,30 @@
 
 void microMouseServer::studentAI()
 {
-  if (isWallLeft() && isWallForward()) {
-        foundFinish();
-    }
-    if (moveForward()==false) {
+  if ((isWallLeft()==true)&&(isWallRight()==true)&&(isWallForward()==true)) {
+        turnLeft(); //making a 180 degree turn
         turnLeft();
-    }
-    else {
-        microMouseServer::studentAI();
-    }
-    
+        moveForward();
+  }
+  else if ((isWallRight()==true)&&(isWallForward()==true)) { //if this condition is met 3 times in a row, the 2x2 square is found
+        turnLeft();
+        moveForward();
+  }
+  else if ((isWallLeft()==true)&&(isWallForward()==true)) { //if this condition is met 3 times in a row, the 2x2 square is found
+        turnRight();
+        moveForward();
+        rTurns++;
+  }
+  else if (isWallRight()==true) {
+        moveForward();
+  }
+  else if (isWallRight()==false) {
+        turnRight();
+        moveForward();
+  }
+  else {
+        microMouseServer::studentAI(); //recursing to find the end of the maze
+  } 
 /*
  * The following are the eight functions that you can call. Feel free to create your own fuctions as well.
  * Remember that any solution that calls moveForward more than once per call of studentAI() will have points deducted.
